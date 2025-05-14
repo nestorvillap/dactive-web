67c59e062730cc75c4962dfd6756195145d9974d
Nestor Villa <nestorvillap@gmail.com>
2025-05-14 23:00:47 +0200

    fix: wrangler pages_build_output_dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,11 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: cloudflare(), // sin opciones extra de modo o carpeta
-  build: {
-    // -- por defecto Astro escribe el server en dist/server
-    //    pero podemos cambiarlo para que salga en ../functions
-    server: "../functions",
-    // Opcional: controlar carpeta de cliente si quieres
-    // client: './client',
-  },
+  adapter: cloudflare(),
 });
